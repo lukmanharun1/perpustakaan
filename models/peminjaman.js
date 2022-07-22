19c48dfaae0cd8e158150cef62a29fb9e150ebb8
Lukman Harun <nandes88.ni@gmail.com>
2022-07-22 15:12:24 +0700

    membuat model peminjaman

---
+++ models/peminjaman.js
@@ -0,0 +1,50 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Peminjaman extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Peminjaman.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+      },
+      tanggal_peminjaman: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      tanggal_pengembalian: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      id_buku: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      id_mahasiswa: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      timestamps: true,
+      modelName: "Peminjaman",
+      tableName: "peminjaman",
+      freezeTableName: true,
+      underscored: true,
+      paranoid: true,
+    }
+  );
+  return Peminjaman;
+};
