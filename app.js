1b1fdf856ac40a4cdbfe7655bb0451e193ada300
Lukman Harun <nandes88.ni@gmail.com>
2022-07-22 13:42:59 +0700

    ubah var jadi const

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+require("dotenv").config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const routes = require("./routes/index");
+const app = express();
 
-var app = express();
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(express.static(path.join(__dirname, "public")));
+app.use(routes);
 
 module.exports = app;
