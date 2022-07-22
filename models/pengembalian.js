13e0fe4edde5b34942d8932ad4b8910769847e14
Lukman Harun <nandes88.ni@gmail.com>
2022-07-22 15:19:35 +0700

    membuat model pengembalian

---
+++ models/pengembalian.js
@@ -0,0 +1,45 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Pengembalian extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Pengembalian.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+      },
+      tanggal_pengembalian: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      id_peminjaman: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      denda: {
+        type: DataTypes.INTEGER,
+      },
+    },
+    {
+      sequelize,
+      timestamps: true,
+      modelName: "Pengembalian",
+      tableName: "pengembalian",
+      freezeTableName: true,
+      underscored: true,
+      paranoid: true,
+    }
+  );
+  return Pengembalian;
+};
