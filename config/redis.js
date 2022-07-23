32e99d7dfa511cc0cc810a5a3ff9d8fa37d06317
Lukman Harun <nandes88.ni@gmail.com>
2022-07-23 12:52:52 +0700

    memperbaiki config redis

--- config/redis.js
+++ config/redis.js
@@ -1,10 +1,15 @@
 const { createClient } = require("redis");
-
 const client = createClient({
   url: process.env.REDIS_URL,
 });
 
-client.on("connect", () => console.log("Redis Client Success Connect"));
-client.on("error", (err) => console.log("Redis Client Error", err));
+client.connect();
+client.on("connect", () => {
+  console.log("Redis Client Success Connect");
+});
+
+client.on("error", (err) => {
+  console.log("Redis Client Error", err);
+});
 
 module.exports = client;
