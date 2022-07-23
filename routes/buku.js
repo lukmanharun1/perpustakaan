a1cc3ee9264808ffb89b84bca166d61b64a71aa4
Lukman Harun <nandes88.ni@gmail.com>
2022-07-23 17:07:31 +0700

    menambahkan routes getById

--- routes/buku.js
+++ routes/buku.js
@@ -4,11 +4,17 @@ const router = express.Router();
 const controller = require("../controller/buku");
 const validate = require("../middleware/validate");
 const validation = require("../validation/buku");
-const { getBukuRedis } = require("../middleware/redis");
+const { getBukuRedis, getBukuByIdRedis } = require("../middleware/redis");
 
 router.get("/", validation.getAll(), validate, getBukuRedis, controller.getAll);
 router.post("/", validation.create(), validate, controller.create);
 router.put("/:id", validation.update(), validate, controller.update);
-router.get("/:id", validation.getById(), validate, controller.getById);
+router.get(
+  "/:id",
+  validation.getById(),
+  validate,
+  getBukuByIdRedis,
+  controller.getById
+);
 
 module.exports = router;
