0ea06f085e23b1788a8815f565406467c46a42cf
Lukman Harun <nandes88.ni@gmail.com>
2022-07-23 17:08:21 +0700

    menambahkan middleware getBukuByIdRedis

--- middleware/redis.js
+++ middleware/redis.js
@@ -15,6 +15,21 @@ const getBukuRedis = async (req, res, next) => {
   next();
 };
 
+const getBukuByIdRedis = async (req, res, next) => {
+  const getBuku = await redisConnection.get(
+    `getBuku:${JSON.stringify(req.params.id)}`
+  );
+  if (getBuku !== null) {
+    return response(res, {
+      status: "success",
+      data: JSON.parse(getBuku),
+    });
+  }
+  console.log("data tidak ada di dalam redis");
+  next();
+};
+
 module.exports = {
   getBukuRedis,
+  getBukuByIdRedis,
 };
