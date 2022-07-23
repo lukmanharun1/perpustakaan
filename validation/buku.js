e44d620dbaa8c4f9f4ffa99b3c6c70328f4f3586
Lukman Harun <nandes88.ni@gmail.com>
2022-07-23 16:11:18 +0700

    menambahkan validation buku (update)

--- validation/buku.js
+++ validation/buku.js
@@ -25,7 +25,20 @@ const create = () => [
   body("nama_rak_buku").notEmpty().isString(),
 ];
 
+const update = () => [
+  body("judul_buku").optional().isString(),
+  body("nama_penulis").optional().isString(),
+  body("nama_penerbit").optional().isString(),
+  body("tahun_penerbit")
+    .optional()
+    .isString()
+    .isLength({ min: 4, max: 4 })
+    .isInt({ min: 1990, max: tahunSekarang }),
+  body("stok").optional().isInt({ min: 1, max: 10000 }),
+];
+
 module.exports = {
   getAll,
   create,
+  update,
 };
