744f1311247f1c3720dca2007e9095ad0a4c245d
Lukman Harun <nandes88.ni@gmail.com>
2022-07-25 10:52:56 +0700

    memperbaiki validation peminjaman

--- validation/peminjaman.js
+++ validation/peminjaman.js
@@ -1,8 +1,16 @@
 const { body } = require("express-validator");
 
 const create = () => [
-  body("tanggal_peminjaman").notEmpty().isDate(),
-  body("tanggal_pengembalian").notEmpty().isDate(),
+  body("tanggal_pengembalian")
+    .notEmpty()
+    .isDate({ format: "YYYY-MM-DD" })
+    .custom((tanggal_pengembalian) => {
+      const tanggalSekarang = new Date().toISOString().split("T")[0];
+      if (tanggalSekarang > tanggal_pengembalian) {
+        throw new Error("Tanggal pengembalian tidak valid!");
+      }
+      return true;
+    }),
   body("id_buku").notEmpty().isUUID(),
   body("id_mahasiswa").notEmpty().isUUID(),
 ];
