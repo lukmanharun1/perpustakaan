c6e1853377cc04dfbc7033ca3f1a984a27796045
Lukman Harun <nandes88.ni@gmail.com>
2022-07-26 18:05:26 +0700

    membuat migration denda

---
+++ migrations/20220722074858-create-denda-table.js
@@ -0,0 +1,41 @@
+"use strict";
+const field = require("../constant/field");
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      "pengembalian",
+      field(Sequelize, {
+        tanggal_pengembalian: {
+          type: Sequelize.DATEONLY,
+        },
+        id_buku: {
+          type: Sequelize.UUID,
+          allowNull: false,
+        },
+        id_mahasiswa: {
+          type: Sequelize.UUID,
+          allowNull: false,
+        },
+        tanggal_jatuh_tempo: {
+          type: Sequelize.DATEONLY,
+        },
+        tanggal_peminjaman: {
+          type: Sequelize.DATEONLY,
+        },
+        status: {
+          type: Sequelize.ENUM(
+            "hilang",
+            "rusak",
+            "terlambat",
+            "hilang dan terlambat",
+            "rusak dan terlambat"
+          ),
+        },
+      })
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("pengembalian");
+  },
+};
