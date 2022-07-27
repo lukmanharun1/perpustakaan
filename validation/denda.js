2352fc852d1757fadacc5167e5cdf87a8739138b
Lukman Harun <nandes88.ni@gmail.com>
2022-07-27 19:16:14 +0700

    membuat validation denda

--- validation/denda.js
+++ validation/denda.js
@@ -1,8 +1,8 @@
 const { body } = require("express-validator");
 
 const create = () => [
-  body("id_buku").notEmpty().isUUID(),
-  body("id_mahasiswa").notEmpty().isUUID(),
+  body("id_buku").isUUID(),
+  body("id_mahasiswa").isUUID(),
   body("status").isIn(["rusak", "hilang"]),
 ];
 
