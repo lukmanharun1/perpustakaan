5cab0091199341cbc13a753769967c841738a4c9
Lukman Harun <nandes88.ni@gmail.com>
2022-07-27 17:08:33 +0700

    menambahkan routes pengembalian

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const buku = require("./buku");
 const peminjaman = require("./peminjaman");
+const pengembalian = require("./pengembalian");
 
 router.use("/buku", buku);
 router.use("/peminjaman", peminjaman);
+router.use("/pengembalian", pengembalian);
 module.exports = router;
