0219af5578b29c0e64b13ad4453e27d270f326da
Lukman Harun <nandes88.ni@gmail.com>
2022-07-27 17:09:00 +0700

    membuat controller pengembalian

---
+++ controller/pengembalian.js
@@ -0,0 +1,101 @@
+const { Peminjaman, Denda, Buku, sequelize, Sequelize } = require("../models");
+const response = require("../helper/response");
+
+const create = async (req, res) => {
+  const transaction = await sequelize.transaction();
+  try {
+    let { tanggal_pengembalian, id_peminjaman, status } = req.body;
+    let isDenda = false;
+    // cari id peminjaman
+    const findPeminjamanById = await Peminjaman.findOne({
+      where: {
+        id: id_peminjaman,
+      },
+      transaction,
+    });
+    if (!findPeminjamanById) {
+      return response(
+        res,
+        {
+          status: "error",
+          message: "Data peminjaman tidak ada!",
+        },
+        404
+      );
+    }
+    const {
+      tanggal_peminjaman,
+      id_buku,
+      id_mahasiswa,
+      tanggal_pengembalian: tanggal_jatuh_tempo,
+    } = findPeminjamanById;
+    if (status) {
+      // create denda
+      isDenda = true;
+    }
+    if (tanggal_pengembalian > findPeminjamanById.tanggal_pengembalian) {
+      // status terlambat | create denda
+      isDenda = true;
+      if (status) {
+        status = `${status} dan terlambat`;
+      } else {
+        status = "terlambat";
+      }
+    }
+    if (isDenda) {
+      // create denda
+      const createDenda = await Denda.create({
+        tanggal_peminjaman,
+        id_buku,
+        id_mahasiswa,
+        tanggal_jatuh_tempo,
+        status,
+        tanggal_pengembalian,
+        transaction,
+      });
+      if (!createDenda) {
+        throw new Error("Data denda gagal dibuat!");
+      }
+    }
+    if (status === "terlambat" || !status) {
+      // update stok buku | tambah stok buku + 1
+      const updateStokBuku = await sequelize.query(
+        `UPDATE buku SET stok = stok + 1 WHERE id = '${id_buku}'`,
+        {
+          model: Buku,
+          type: Sequelize.QueryTypes.UPDATE,
+          transaction,
+        }
+      );
+      if (!updateStokBuku) {
+        throw new Error("Data stok buku gagal diupdate!");
+      }
+    }
+    // hapus data peminjaman
+    const deletePeminjaman = await findPeminjamanById.destroy({ transaction });
+    if (!deletePeminjaman) {
+      throw new Error("Data peminjaman gagal dihapus!");
+    }
+    // commit transaction
+    await transaction.commit();
+    return response(res, {
+      status: "success",
+      message: `Data pengembalian berhasil diproses!`,
+      status_denda: status,
+    });
+  } catch (error) {
+    await transaction.rollback();
+    return response(
+      res,
+      {
+        status: "error",
+        message: error.message,
+      },
+      500
+    );
+  }
+};
+
+module.exports = {
+  create,
+};
