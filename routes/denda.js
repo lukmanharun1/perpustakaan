680f912ecc7f24c3f5eed007964c44b5e012c75d
Lukman Harun <nandes88.ni@gmail.com>
2022-07-27 18:14:58 +0700

    membuat routes denda

---
+++ routes/denda.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+const controller = require("../controller/denda");
+const validate = require("../middleware/validate");
+const validation = require("../validation/denda");
+
+router.post("/", validation.create(), validate, controller.create);
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const buku = require("./buku");
 const peminjaman = require("./peminjaman");
 const pengembalian = require("./pengembalian");
+const denda = require("./denda");
 
 router.use("/buku", buku);
 router.use("/peminjaman", peminjaman);
 router.use("/pengembalian", pengembalian);
+router.use("/denda", denda);
 module.exports = router;
