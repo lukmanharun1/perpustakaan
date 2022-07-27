a22886440bf8b4640f3a2b5cc3e3c340f5e3683a
Lukman Harun <nandes88.ni@gmail.com>
2022-07-27 19:08:35 +0700

    menambahkan relationship peminjaman (model

--- models/peminjaman.js
+++ models/peminjaman.js
@@ -9,6 +9,14 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Peminjaman.belongsTo(models.Mahasiswa, {
+        foreignKey: "id_mahasiswa",
+        as: "mahasiswa",
+      });
+      Peminjaman.belongsTo(models.Buku, {
+        foreignKey: "id_buku",
+        as: "buku",
+      });
     }
   }
   Peminjaman.init(
