4111f464c0a379d5e3d768c9b412ef7767beea73
Lukman Harun <nandes88.ni@gmail.com>
2022-07-27 17:08:04 +0700

    memperbaiki validation pengembalian

--- validation/pengembalian.js
+++ validation/pengembalian.js
@@ -3,6 +3,7 @@ const { body } = require("express-validator");
 const create = () => [
   body("tanggal_pengembalian").notEmpty().isDate({ format: "YYYY-MM-DD" }),
   body("id_peminjaman").notEmpty().isUUID(),
+  body("status").optional().isIn(["rusak", "hilang"]),
 ];
 
 module.exports = {
