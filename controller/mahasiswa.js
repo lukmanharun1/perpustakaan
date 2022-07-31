0fcdabff7fcd49ba7664c906ce8263083ec7327f
Lukman Harun <nandes88.ni@gmail.com>
2022-07-31 19:05:27 +0700

    menambahkan controller getById (mahasiswa)

--- controller/mahasiswa.js
+++ controller/mahasiswa.js
@@ -2,10 +2,11 @@ const { Mahasiswa } = require("../models");
 const response = require("../helper/response");
 const redisConnection = require("../config/redis");
 
+const attributes = ["id", "jurusan", "no_telp", "alamat", "nama_lengkap"];
 const getAll = async (req, res) => {
   try {
     const getAllMahasiswa = await Mahasiswa.findAll({
-      attributes: ["id", "jurusan", "no_telp", "alamat", "nama_lengkap"],
+      attributes,
     });
     if (getAllMahasiswa.length > 0) {
       // set getMahasiswa: redis
@@ -31,6 +32,52 @@ const getAll = async (req, res) => {
   }
 };
 
+const getById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    // ambil data di redis jika ada
+    const getMahasiswaRedis = await redisConnection.get("getMahasiswa:");
+    if (getMahasiswaRedis !== null) {
+      const resultArray = JSON.parse(getMahasiswaRedis);
+      const getMahasiswaById = resultArray.find((data) => data.id === id);
+      if (getMahasiswaById) {
+        return response(res, {
+          status: "success",
+          data: getMahasiswaById,
+        });
+      } else {
+        throw {
+          message: "Data mahasiswa tidak ada!",
+          statusCode: 404,
+        };
+      }
+    }
+    const getMahasiswaById = await Mahasiswa.findByPk(id, {
+      attributes,
+    });
+    if (!getMahasiswaById) {
+      throw {
+        message: "Data mahasiswa tidak ada!",
+        statusCode: 404,
+      };
+    }
+    return response(res, {
+      status: "success",
+      data: getMahasiswaById,
+    });
+  } catch (error) {
+    return response(
+      res,
+      {
+        status: "error",
+        message: error.message,
+      },
+      error.statusCode || 500
+    );
+  }
+};
+
 module.exports = {
   getAll,
+  getById,
 };
