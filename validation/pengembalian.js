da9b9432e978f971e0499c85fcf58427a3c28dc0
Lukman Harun <nandes88.ni@gmail.com>
2022-08-11 13:36:28 +0700

    menambakahn validation nominal

--- validation/pengembalian.js
+++ validation/pengembalian.js
@@ -4,6 +4,15 @@ const create = () => [
   body("tanggal_pengembalian").notEmpty().isDate({ format: "YYYY-MM-DD" }),
   body("id_peminjaman").notEmpty().isUUID(),
   body("status").optional().isIn(["rusak", "hilang"]),
+  body("nominal")
+    .optional()
+    .isInt({ min: 500 })
+    .custom((nominal, { req }) => {
+      if (req.status && !nominal) {
+        throw new Error("nominal wajib diisi!");
+      }
+      return true;
+    }),
 ];
 
 module.exports = {
