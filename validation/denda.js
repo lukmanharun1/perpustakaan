9240e25f0bb624688b05f56d41a659b1d88b63c9
Lukman Harun <nandes88.ni@gmail.com>
2022-08-11 18:52:02 +0700

    include validation nominal

--- validation/denda.js
+++ validation/denda.js
@@ -1,9 +1,10 @@
 const { body } = require("express-validator");
-
+const nominal = require("./custom/nominal");
 const create = () => [
   body("id_buku").isUUID(),
   body("id_mahasiswa").isUUID(),
   body("status").isIn(["rusak", "hilang"]),
+  body("nominal").custom(nominal),
 ];
 
 module.exports = {
--- validation/pengembalian.js
+++ validation/pengembalian.js
@@ -1,21 +1,11 @@
 const { body } = require("express-validator");
+const nominal = require("./custom/nominal");
 
 const create = () => [
   body("tanggal_pengembalian").notEmpty().isDate({ format: "YYYY-MM-DD" }),
   body("id_peminjaman").notEmpty().isUUID(),
   body("status").optional().isIn(["rusak", "hilang"]),
-  body("nominal").custom((nominal, { req }) => {
-    if (req.body.status && !nominal) {
-      throw new Error("nominal wajib diisi!");
-    } else if (nominal) {
-      if (typeof nominal !== "number") {
-        throw new Error("nominal harus number!");
-      } else if (nominal < 500) {
-        throw new Error("nominal minimal haru 500");
-      }
-    }
-    return true;
-  }),
+  body("nominal").custom(nominal),
 ];
 
 module.exports = {
