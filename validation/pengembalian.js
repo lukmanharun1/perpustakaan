5c128d1a7802b71012378dfeb9dc01324aaa8be4
Lukman Harun <nandes88.ni@gmail.com>
2022-08-11 14:02:05 +0700

    fix validation nominal

--- validation/pengembalian.js
+++ validation/pengembalian.js
@@ -4,15 +4,16 @@ const create = () => [
   body("tanggal_pengembalian").notEmpty().isDate({ format: "YYYY-MM-DD" }),
   body("id_peminjaman").notEmpty().isUUID(),
   body("status").optional().isIn(["rusak", "hilang"]),
-  body("nominal")
-    .optional()
-    .isInt({ min: 500 })
-    .custom((nominal, { req }) => {
-      if (req.status && !nominal) {
-        throw new Error("nominal wajib diisi!");
-      }
-      return true;
-    }),
+  body("nominal").custom((nominal, { req }) => {
+    if (req.body.status && !nominal) {
+      throw new Error("nominal wajib diisi!");
+    } else if (typeof nominal !== "number") {
+      throw new Error("nominal harus number!");
+    } else if (nominal < 500) {
+      throw new Error("nominal minimal haru 500");
+    }
+    return true;
+  }),
 ];
 
 module.exports = {
