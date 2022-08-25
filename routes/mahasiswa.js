5b69a58b9aec4fa6abd7bef03984f779a9fa3f79
Lukman Harun <nandes88.ni@gmail.com>
2022-08-25 12:57:55 +0700

    menambahkan route (delete) mahasiswa

--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -10,5 +10,6 @@ router.get("/", getMahasiswaRedis, controller.getAll);
 router.get("/:id", validation.getById(), validate, controller.getById);
 router.post("/", validation.create(), validate, controller.create);
 router.put("/:id", validation.update(), validate, controller.update);
+router.delete("/:id", validation.destroy(), validate, controller.destroy);
 
 module.exports = router;
