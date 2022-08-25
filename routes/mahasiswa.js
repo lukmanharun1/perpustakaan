219df794ce5295cde2c3cebd01a88c9bb775af6b
Lukman Harun <nandes88.ni@gmail.com>
2022-08-25 11:06:17 +0700

    menambahkan route (create) mahasiswa

--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -8,5 +8,6 @@ const validation = require("../validation/mahasiswa");
 
 router.get("/", getMahasiswaRedis, controller.getAll);
 router.get("/:id", validation.getById(), validate, controller.getById);
+router.post("/", validation.create(), validate, controller.create);
 
 module.exports = router;
