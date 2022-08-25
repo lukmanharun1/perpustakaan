5f39d9509f5a45eab984c456d0320950671f857e
Lukman Harun <nandes88.ni@gmail.com>
2022-08-25 12:56:19 +0700

    menambahkan validation (update) mahasiswa

--- validation/mahasiswa.js
+++ validation/mahasiswa.js
@@ -9,7 +9,20 @@ const create = () => [
   body("nama_lengkap").isString().isLength({ min: 3, max: 128 }),
 ];
 
+const update = () => [
+  param("id").notEmpty().isUUID(),
+  body("jurusan").optional().isString().isLength({ min: 2, max: 128 }),
+  body("no_telp")
+    .optional()
+    .isString()
+    .isNumeric()
+    .isLength({ min: 11, max: 13 }),
+  body("alamat").optional().isString().isLength({ min: 20, max: 255 }),
+  body("nama_lengkap").optional().isString().isLength({ min: 3, max: 128 }),
+];
+
 module.exports = {
   getById,
   create,
+  update,
 };
